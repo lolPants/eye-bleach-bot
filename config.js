41597012713ffbf83a907188f75f25b8c0187329
Jack Baron <yodude107@gmail.com>
2016-05-18 17:30:17 +0100

    For some reason I need the config to do CI

---
+++ config.js
@@ -0,0 +1,10 @@
+// CONFIG
+module.exports = {
+  subreddit:    '',
+  twit_conf: {
+    consumer_key:         '',
+    consumer_secret:      '',
+    access_token:         '',
+    access_token_secret:  ''
+  }
+};
