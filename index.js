0769e531875134bfa8f73ad3a523aa159d003838
Jack Baron <yodude107@gmail.com>
2016-05-18 17:54:55 +0100

    Add dat logging to the main file :D

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var func_r = require('./eyebleach_reddit');
 var func_t = require('./eyebleach_twitter');
+var func_help = require('./eyebleach_help');
 var conf = require('./config');
 
 function postEyeBleach() {
   func_t.postTweet(func_r.getUrl(conf.subreddit)[1], func_r.getUrl(conf.subreddit)[0]);
-  console.log("Eyebleach Posted");
+  func_help.log("Eyebleach Posted");
 }
 
 // Run to start
