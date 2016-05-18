07675d0c2b055a7fb630c84e5e8a89da559fe01c
Jack Baron <yodude107@gmail.com>
2016-05-18 12:41:11 +0100

    TWITTER WORKS \o/

--- eyebleach_twitter.js
+++ eyebleach_twitter.js
@@ -14,13 +14,28 @@ function postTweet(url, credit) {
       if (err) {
           console.log(err);
       }
-      console.log(image);
+      //console.log(image);
       // Now we have the image.
       // Lets upload that shit to twitter
   
-      payload = url + "\nCredit to /u/" + global.credits + " on Reddit"
-      T.post('statuses/update', { status: payload }, function(err, data, response) {
-        //console.log(data)
+      payload = "Credit to /u/" + global.credits + " on Reddit"
+      T.post('media/upload', { media_data: image }, function (err, data, response) {
+        // now we can assign alt text to the media, for use by screen readers and
+        // other text-based presentations and interpreters
+        var mediaIdStr = data.media_id_string;
+        var altText = "Eyebleach Image. Credit to /u/" + global.credits;
+        var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } };
+
+        T.post('media/metadata/create', meta_params, function (err, data, response) {
+          if (!err) {
+            // now we can reference the media and post a tweet (media will attach to the tweet)
+            var params = { status: payload, media_ids: [mediaIdStr] }
+
+            T.post('statuses/update', params, function (err, data, response) {
+              //console.log(data)
+            })
+          }
+        })
       });
   });
 }
