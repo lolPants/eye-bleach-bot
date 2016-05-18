95c57bcfa0ee9a62f7a2c367f6cc5d28f9fe790d
Jack Baron <yodude107@gmail.com>
2016-05-18 17:26:58 +0100

    Whoopsy-daisy

--- tests.js
+++ tests.js
@@ -1,7 +1,7 @@
 var func_r = require('./eyebleach_reddit');
 var func_t = require('./eyebleach_twitter');
 
-if (func_r.getUrl() != null) {
+if (func_r.getUrl("aww", 100) != null) {
   console.log('URL Pulling [OK]');
 } else {
   throw Error('Replace Function Failed');
