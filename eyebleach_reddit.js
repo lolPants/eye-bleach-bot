0f8af1c2db666efec844cb1c1eeffd7890deb01e
Jack Baron <yodude107@gmail.com>
2016-05-18 12:56:20 +0100

    God damn tab indentation

--- eyebleach_reddit.js
+++ eyebleach_reddit.js
@@ -3,15 +3,15 @@ var request = require('sync-request');
 
 // Function to, get this, GET some DATA :O
 function getData(subreddit, count) {
-    // What URL are we gonna use? Compile one from some arguments
-    url = "https://www.reddit.com/r/" + subreddit + "/top.json?limit=" + count;
-    // Make a magical GET request
-    var res = request("GET", url);
-    // Parse into a format we can actually work with
-    var data = JSON.parse(res.getBody('utf8'));
-    // Send back the data
-    return data['data']['children'];
-  }
+  // What URL are we gonna use? Compile one from some arguments
+  url = "https://www.reddit.com/r/" + subreddit + "/top.json?limit=" + count;
+  // Make a magical GET request
+  var res = request("GET", url);
+  // Parse into a format we can actually work with
+  var data = JSON.parse(res.getBody('utf8'));
+  // Send back the data
+  return data['data']['children'];
+}
 
 // Shove these functions to the rest of the bot. SHOVE HARDER!
 module.exports = {
