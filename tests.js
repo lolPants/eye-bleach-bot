c702511f30ebcff94248d908d6f54b321ebbccd3
Jack Baron <yodude107@gmail.com>
2016-05-18 17:44:52 +0100

    Add Logging (WIP)

--- tests.js
+++ tests.js
@@ -1,7 +1,10 @@
 var func_r = require('./eyebleach_reddit');
+var func_help = require('./eyebleach_help');
 
 if (func_r.getUrl("aww", 100) != null) {
   console.log('URL Pulling [OK]');
 } else {
   throw Error('Replace Function Failed');
 }
+
+func_help.log('Testing Logs');
